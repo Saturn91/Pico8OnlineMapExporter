7cb7dc99cb5a66b085e8bb22fe13d0c92b516235
Manuel G <manuel.geissberger@gmail.com>
2022-03-27 18:13:46 +0200

    [ADD] on data reload clear sprites

--- scripts/parseSpriteData.service.js
+++ scripts/parseSpriteData.service.js
@@ -2,6 +2,7 @@ const sprites = undefined;
 
 function drawSpriteDataToCanvas(ctx, spriteData, scale) {
     const lines = spriteData.split(/\r?\n/);
+    ctx.clearRect(0, 0, 16*8, 16*8);    
     ctx.scale(scale,scale);
     for(let x = 0; x < lines[1].length; x++) {
         for(let y = 0; y < lines.length; y++) {
