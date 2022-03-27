c04d027dac90daba08ab773b9a991525ed52cc0d
Manuel G <manuel.geissberger@gmail.com>
2022-03-27 15:26:39 +0200

    [ADD] controls, select dualsprite area which will be used

--- scripts/drawMap.service.js
+++ scripts/drawMap.service.js
@@ -7,7 +7,7 @@ function drawMap(mapCtx, sprites, singlePurposeMapData, dualPurposeGFXData1, dua
             const spriteX = parseHex(lines[y][x*2+1]);
             if(spriteX || spriteY) mapCtx.drawImage(sprites.canvas, spriteX*8, spriteY*8, 8, 8, x*8, y*8, 8, 8);
             else {
-                mapCtx.fillStale = PICO_8_COLOR_PALLETE[0];
+                mapCtx.fillStyle = PICO_8_COLOR_PALLETE[0];
                 mapCtx.fillRect(x*8,y*8, 8, 8);
             }
         }
@@ -24,21 +24,24 @@ function drawMap(mapCtx, sprites, singlePurposeMapData, dualPurposeGFXData1, dua
                 if(yIsOdd) {
                     if(spriteX || spriteY) mapCtx.drawImage(sprites.canvas, spriteX*8, spriteY*8, 8, 8, x*8, 256+y/2*8, 8, 8);
                     else {
-                        mapCtx.fillStale = PICO_8_COLOR_PALLETE[0];
+                        mapCtx.fillStyle = PICO_8_COLOR_PALLETE[0];
                         mapCtx.fillRect(x*8,256+y/2*8, 8, 8);
                     }
                 } else {
                     if(spriteX || spriteY) mapCtx.drawImage(sprites.canvas, spriteX*8, spriteY*8, 8, 8, 512+x*8, 256+(y-1)/2*8, 8, 8);
                     else {
-                        mapCtx.fillStale = PICO_8_COLOR_PALLETE[0];
+                        mapCtx.fillStyle = PICO_8_COLOR_PALLETE[0];
                         mapCtx.fillRect(512+x*8,256+(y-1)/2*8, 8, 8);
                     }
                 }            
             }
         }
+    } else {
+        mapCtx.fillStyle = PICO_8_COLOR_PALLETE[0];
+        mapCtx.fillRect(0,256, 32*4*8, 4*4*8);
     }
     
-    if(dualPurposeGFXData1 && dualPurposeGFXData2) {
+    if(dualPurposeGFXData2) {
         lines = dualPurposeGFXData2.split(/\r?\n/);
         mapCtx.scale(scale,scale); 
         for(let x = 0; x < lines[1].length/2; x++) {
@@ -49,17 +52,20 @@ function drawMap(mapCtx, sprites, singlePurposeMapData, dualPurposeGFXData1, dua
                 if(!yIsOdd) {
                     if(spriteX || spriteY) mapCtx.drawImage(sprites.canvas, spriteX*8, spriteY*8, 8, 8, x*8, 380+y/2*8, 8, 8);
                     else {
-                        mapCtx.fillStale = PICO_8_COLOR_PALLETE[0];
+                        mapCtx.fillStyle = PICO_8_COLOR_PALLETE[0];
                         mapCtx.fillRect(x*8,380+y/2*8, 8, 8);
                     }
                 } else {
                     if(spriteX || spriteY) mapCtx.drawImage(sprites.canvas, spriteX*8, spriteY*8, 8, 8, 512+x*8, 256+120+y/2*8, 8, 8);
                     else {
-                        mapCtx.fillStale = PICO_8_COLOR_PALLETE[0];
+                        mapCtx.fillStyle = PICO_8_COLOR_PALLETE[0];
                         mapCtx.fillRect(512+x*8,256+120+y/2*8, 8, 8);
                     }
                 }            
             }
         }
+    } else {
+        mapCtx.fillStyle = PICO_8_COLOR_PALLETE[0];
+        mapCtx.fillRect(0,380, 32*4*8, 4*4*8);
     }
 }
--- scripts/index.js
+++ scripts/index.js
@@ -4,21 +4,27 @@ function submitPico8SourceFile() {
     loadFromFile(fileupload.files[0], (text) => processPico8RawData(text), (error) => console.error(error));
 }
 
+let spriteData = undefined;
+let dualPurposeGFXData1 = undefined;
+let dualPurposeGFXData2 = undefined;
+let singlePurposeMapData = undefined;
+
 function processPico8RawData(data) {
     mapDataStartIndex = data.indexOf("__map__") + 8;
-    const singlePurposeMapData = data.substring(mapDataStartIndex, mapDataStartIndex + 128 * 32 * 2 + 32);
+    singlePurposeMapData = data.substring(mapDataStartIndex, mapDataStartIndex + 128 * 32 * 2 + 32);
 
     spriteDataStartIndex = data.indexOf("__gfx__") + 8;
-    const spriteData = data.substring(spriteDataStartIndex, spriteDataStartIndex + 128 * 32 * 2 + 64);
+    spriteData = data.substring(spriteDataStartIndex, spriteDataStartIndex + 128 * 32 * 2 + 64);
 
     dualGFXPurposeStartIndex = spriteDataStartIndex + 128 * 32 * 2 + 64;
-    const dualPurposeGFXData1 = data.substring(dualGFXPurposeStartIndex, dualGFXPurposeStartIndex + 128 * 32 + 32);
-    const dualPurposeGFXData2 = data.substring(dualGFXPurposeStartIndex + 128 * 32, dualGFXPurposeStartIndex + 128 * 32 + 128 * 32 + 64);
+    dualPurposeGFXData1 = data.substring(dualGFXPurposeStartIndex, dualGFXPurposeStartIndex + 128 * 32 + 32);
+    dualPurposeGFXData2 = data.substring(dualGFXPurposeStartIndex + 128 * 32, dualGFXPurposeStartIndex + 128 * 32 + 128 * 32 + 64);
 
     drawSpriteDataToCanvas(singlePorpuseSpritesCtx, spriteData, displayScale);
     drawSpriteDataToCanvas(dualPorpuseSpritesCtx1,  dualPurposeGFXData1, displayScale);
     drawSpriteDataToCanvas(dualPorpuseSpritesCtx2,  dualPurposeGFXData2, displayScale);
-    drawMap(canvasMapCtx, singlePorpuseSpritesCtx, singlePurposeMapData, dualPurposeGFXData1, dualPurposeGFXData2, 1);
+    previewMapDataBtn.disabled = !spriteData;
+    
 }
 
 const fileupload = document.getElementById('pico8-source-upload');
@@ -44,3 +50,18 @@ const canvasMap = document.getElementById('canvas-map');
 const canvasMapCtx = canvasMap.getContext("2d");
 canvasMap.width = 8*16*8;
 canvasMap.height = 4*16*8;
+
+//define controlls
+const previewMapDataBtn = document.getElementById('preview-map-data-btn');
+previewMapDataBtn.disabled = true;
+previewMapDataBtn.addEventListener('click', () => {
+    selectedDualpurpose1 = checkBoxUseDualPurpose1.checked ? dualPurposeGFXData1 : undefined;
+    selectedDualpurpose2 = checkBoxUseDualPurpose2.checked ? dualPurposeGFXData2 : undefined;
+    drawMap(canvasMapCtx, singlePorpuseSpritesCtx, singlePurposeMapData, selectedDualpurpose1, selectedDualpurpose2, 1);
+});
+
+let selectedDualpurpose1 = undefined;
+let selectedDualpurpose2 = undefined;
+
+const checkBoxUseDualPurpose1 = document.getElementById("use_dual_purpose1");
+const checkBoxUseDualPurpose2 = document.getElementById("use_dual_purpose2");
